262ada2286fc97aacf2f814cae067f8aedc9da4c
jairusbp <pur18007@byui.edu>
2019-02-17 08:50:58 +0800

    commit

---
+++ assignments/lesson-6/scripts/map.js
@@ -0,0 +1,8 @@
+
+ function initMap() {
+  var mapProp= {
+    center:new google.maps.LatLng(42.0963,111.8766),
+    zoom:5,
+  };
+  var map = new google.maps.Map(document.getElementById("map"),mapProp);
+  }
